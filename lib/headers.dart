b8ffefb2eeca7e3945ee92a875e20f41bb17baa8
salmannaeem112215 <salman.naeem.112215@gmail.com>
2023-06-16 22:56:54 +0500

    Layout Set

--- lib/headers.dart
+++ lib/headers.dart
@@ -2,14 +2,14 @@ export 'dart:async';
 export 'dart:convert';
 export 'dart:typed_data';
 
+export 'package:get/get.dart';
 export 'package:flutter/services.dart';
 export 'package:flutter/material.dart';
-export 'package:get/get.dart';
-export 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
-export 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+export 'package:flutter/foundation.dart';
 export 'package:qr_flutter/qr_flutter.dart';
 export 'package:get/get_instance/src/bindings_interface.dart';
-export 'package:flutter/foundation.dart';
+export 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+export 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 export './routes/app_pages.dart';
 export './constants/icons_path.dart';
@@ -20,13 +20,21 @@ export './modules/qr/binding/qr_binding.dart';
 
 export './modules/home/binding/home_binding.dart';
 export './modules/home/controller/home_controller.dart';
-export './modules/home/view/home_screen.dart';
+export 'modules/home/screen/home_screen.dart';
 export './modules/home/view/home_tile.dart';
 export './modules/home/view/bluetooth_disable.dart';
 export './modules/home/view/bluetooth_enable.dart';
 
-export './modules/chat/model/message.dart';
-export './modules/chat/controller/chat_controller.dart';
-export './modules/chat/view/chat_screen.dart';
 export './modules/chat/binding/chat_binding.dart';
+export './modules/chat/controller/chat_controller.dart';
+export './modules/chat/model/message.dart';
+export './modules/chat/screen/chat_screen.dart';
+export './modules/chat/view/message_box.dart';
+export './modules/chat/view/message_list.dart';
+export './modules/chat/view/no_message.dart';
 export './modules/chat/view/message_tile.dart';
+
+export './modules/image/binding/image_binding.dart';
+export './modules/image/controller/image_controller.dart';
+export './modules/image/screen/image_screen.dart';
+export './modules/image/view/view1.dart';
